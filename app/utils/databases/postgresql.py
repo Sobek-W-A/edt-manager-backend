3e88b4cd54ca784504f0b9c3d821387a5186b5cf
Maxime Marcelin <maisonmarc7@gmail.com>
2024-10-22 12:44:40 +0200

    Fixed issues with Postgres initialization.

--- app/utils/databases/postgresql.py
+++ app/utils/databases/postgresql.py
@@ -62,7 +62,7 @@ class Postgresql:
         It also initialises the models if they are not already present.
         """
         # Fetching basic data
-        url = Postgresql.get_db_url()
+        url    = Postgresql.get_db_url()
         models = Postgresql.get_available_models()        # Listing models available.
 
         # Initialize Tortoise ORM
