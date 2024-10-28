55749f4cb0c2523413c84b06c3083c4f9cde72d8
HugoBien <hug.bien@gmail.com>
2024-10-28 15:56:09 +0100

    Début route add_user

--- app/routes/user.py
+++ app/routes/user.py
@@ -8,11 +8,23 @@ from app.models.tortoise.user import CreateUserInDB, UserInDB
 userRouter: APIRouter = APIRouter()
 
 @userRouter.post("/create")
-async def create_user(form_data : CreateUserInDB):
+async def create_user(body : CreateUserInDB):
     """
     This method create a new user.
     """
 
+    if body.hash is None:
+        print('ici on va generer un mdp aleatoire')
+
+    print("modifier la création de l'id")
+    user = await UserInDB.create(
+        id=1,
+        login=body.login,
+        firstname=body.firstname,
+        lastname=body.lastname,
+        mail=body.mail,
+        hash=body.hash
+    )
 
 
-    return None
+    return user
