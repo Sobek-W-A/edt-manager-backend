c84660e786423670ec35a18af23316482c368194
Maxime Marcelin <maisonmarc7@gmail.com>
2024-10-31 19:09:47 +0100

    Fixed modify user route.

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,7 +1,7 @@
 """
 This module provieds a router for the /user endpoint.
 """
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 from app.models.pydantic.UserModel import PydanticUserModify
 
@@ -10,8 +10,9 @@ import app.services.UserService as UserService
 userRouter: APIRouter = APIRouter(prefix="/user")
 
 @userRouter.patch("/{user_id}", status_code=205)
-async def modify_user(user_id: int, user_model: PydanticUserModify) -> None:
+async def modify_user(user_id: int, user_model: PydanticUserModify) -> Response:
     """
     This controllers is used when modifying user informations.
     """
     await UserService.modify_user(user_id, user_model)
+    return Response(status_code=205)
