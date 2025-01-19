712c6366879e1fe02750edc0636c6c1fef7a0e19
Hugo <hug.bien@gmail.com>
2025-01-19 16:23:33 +0100

    CRUD UE terminé + get course by ID

--- app/services/CourseService.py
+++ app/services/CourseService.py
@@ -6,6 +6,7 @@ from app.models.pydantic.CourseTypeModel import PydanticCourseTypeModel
 from app.models.tortoise.course import CourseInDB
 
 from app.utils.enums.http_errors import CommonErrorMessages
+from app.utils.enums.permission_enums import AvailableServices, AvailableOperations
 
 
 async def get_course_by_id(course_id: int) -> PydanticCourseModel:
@@ -13,27 +14,24 @@ async def get_course_by_id(course_id: int) -> PydanticCourseModel:
         This method retrieves course from id.
         """
 
-    #TODO
-
-    #PAS FONCTIONNEL ENCORE
-
-    course: CourseInDB = await CourseInDB.get_or_none(id=course_id)
+    course: CourseInDB = await CourseInDB.get_or_none(id=course_id).prefetch_related("course_type")
 
     if course is None:
         raise HTTPException(status_code=404, detail=CommonErrorMessages.COURSE_NOT_FOUND.value)
 
     course_type = PydanticCourseTypeModel(
+        academic_year=course.course_type.academic_year,
         course_type_id=course.course_type.id,
         name=course.course_type.name,
-        description=course.course_type.description,
-        academic_year=course.course_type.academic_year
+        description=course.course_type.description
+
     )
 
     return PydanticCourseModel(academic_year=course.academic_year,
+                               id=course.id,
                                duration=course.duration,
-                               course_id=course.id,
                                group_count=course.group_count,
-                               courses_type=[course_type])
+                               course_type=[course_type])
 
 
 async def add_course(body: PydanticCourseModel) -> PydanticCourseModel:
