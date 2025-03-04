88a346768a1386ac9159ee0327c3b25389455521
Maxime Marcelin <maisonmarc7@gmail.com>
2025-03-04 10:14:48 +0100

    [WIP] Added basic enums for more complete permissions

--- app/services/PermissionService.py
+++ app/services/PermissionService.py
@@ -6,10 +6,9 @@ a certain operation on a certain service.
 from fastapi import HTTPException
 from app.models.tortoise.account import AccountInDB
 from app.models.tortoise.account_metadata import AccountMetadataInDB
-from app.models.tortoise.permission import PermissionInDB
 from app.models.tortoise.role import RoleInDB
 from app.utils.enums.http_errors import CommonErrorMessages
-from app.utils.enums.permission_enums import AvailableOperations, AvailableServices
+from app.utils.enums.permission_enums import AvailableOperations, AvailableRoles, AvailableServices
 
 
 async def check_permissions(service: AvailableServices,
@@ -32,12 +31,23 @@ async def check_permissions(service: AvailableServices,
     if role is None:
         raise HTTPException(status_code=403, detail=CommonErrorMessages.FORBIDDEN_ACTION)
 
-    # We fetch the permissions of the role.
-    # We also filter the permissions to get only the ones that match the service and the operation.
-    permissions: list[PermissionInDB] = await role.permissions.filter(service_id=service.value.service_name,
-                                                                      operation_id=operation.value.operation_name)
+    # Admin case
+    # Admin has all the permissions, always.
+    if role.name == AvailableRoles.ADMIN.value.role_name:
+        return
+
+    # Department Manager case.
+    # Same permission as admin, but for only one academic_year.
+    if role.name == AvailableRoles.DPT_MANAGER.value.role_name:
+        if meta.academic_year == academic_year:
+            return
+        else:
+            raise HTTPException(status_code=403,
+                                detail=CommonErrorMessages.FORBIDDEN_ACTION)
 
     # If the list is empty, the user does not have the permission.
     # Otherwise, the user has the permission to do the operation on the service.
-    if len(permissions) == 0:
+    permission_count : int = await role.permissions.filter(service_id=service.value.service_name,
+                                                           operation_id=operation.value.operation_name).count()
+    if permission_count == 0:
         raise HTTPException(status_code=403, detail=CommonErrorMessages.FORBIDDEN_ACTION)
--- app/utils/enums/permission_enums.py
+++ app/utils/enums/permission_enums.py
@@ -4,7 +4,6 @@ These are basically static data that needs to be stored in database at creation.
 They are used to check if a user has the permission to perform a certain operation 
 on a certain service.
 """
-import enum
 
 from app.models.tortoise.operation import OperationInDB
 from app.models.tortoise.permission import PermissionInDB
@@ -100,13 +99,15 @@ class Role(LoadableData):
 
         if self.admin:
             for permission in AvailablePermissions:
-                perms: list[PermissionInDB] = await PermissionInDB.all().filter(service=permission.value.service.service_name)
+                perms: list[PermissionInDB] = await PermissionInDB.all()\
+                                                                  .filter(service=permission.value.service.service_name)
                 await role.permissions.add(*perms)
         else:
             if self.permissions is not None :
                 for permission in self.permissions:
-                    perms: list[PermissionInDB] = await PermissionInDB.all().filter(service=permission.service.service_name,
-                                                                                    operation__in=[op.operation_name for op in permission.operations])
+                    perms: list[PermissionInDB] = await PermissionInDB.all()\
+                                                                      .filter(service=permission.service.service_name,
+                                                                              operation__in=[op.operation_name for op in permission.operations])
                     await role.permissions.add(*perms)
 
 # Enums
@@ -114,24 +115,41 @@ class AvailableOperations(AbstractEnumLoader):
     """
     Enumeration to provide the available CRUD operations on the available services.
     """
-    CREATE = Operation("Create", "Operations that correspond to the creation of a resource")
-    GET    = Operation("Get",    "Operations that correspond to the retrieval of a resource")
-    UPDATE = Operation("Update", "Operations that correspond to an update of a resource")
-    DELETE = Operation("Delete", "Operations that correspond to the deletion of a resource")
+    CREATE       = Operation("Create",
+                             "Operations that correspond to the creation of a resource")
+    GET_MULTIPLE = Operation("Get Multiple",
+                             "Operations that correspond to the retrieval of multiple resources")
+    GET_SINGLE   = Operation("Get Single",
+                             "Operations that correspond to the retrieval of a single resource")
+    GET_ME       = Operation("Get Me",
+                             "Operations that correspond to the retrieval of data about the current user")
+    UPDATE       = Operation("Update",
+                             "Operations that correspond to an update of a resource")
+    DELETE       = Operation("Delete",
+                             "Operations that correspond to the deletion of a resource")
 
 class AvailableServices(AbstractEnumLoader):
     """
     Enumeration to provide the available services.
     """
-    PROFILE_SERVICE     = Service("Profile Service", "Service that manages profiles.")
-    ACCOUNT_SERVICE     = Service("Account Service", "Service that manages accounts and logging.")
-    AFFECTATION_SERVICE = Service("Affectation Service", "Service that manages course to teachers affectations.")
-    STATUS_SERVICE      = Service("Status Service",  "Service that manages statuses.")
-    UE_SERVICE          = Service("UE Service",      "Service that manages Learning Units.")
-    NODE_SERVICE        = Service("Node Service",    "Service that manages nodes.")
-    ROLE_SERVICE        = Service("Role Service",    "Service that manages roles.")
-    COURSE_SERVICE      = Service("Course Service", "Service that manages courses.")
-    ACADEMIC_YEAR_SERVICE = Service("Academic Year Service", "Service that manages academic years.")
+    PROFILE_SERVICE       = Service("Profile Service",
+                                    "Service that manages profiles.")
+    ACCOUNT_SERVICE       = Service("Account Service",
+                                    "Service that manages accounts and logging.")
+    AFFECTATION_SERVICE   = Service("Affectation Service",
+                                    "Service that manages course to teachers affectations.")
+    STATUS_SERVICE        = Service("Status Service",
+                                    "Service that manages statuses.")
+    UE_SERVICE            = Service("UE Service",
+                                    "Service that manages Learning Units.")
+    NODE_SERVICE          = Service("Node Service",
+                                    "Service that manages nodes.")
+    ROLE_SERVICE          = Service("Role Service",
+                                    "Service that manages roles.")
+    COURSE_SERVICE        = Service("Course Service",
+                                    "Service that manages courses.")
+    ACADEMIC_YEAR_SERVICE = Service("Academic Year Service",
+                                    "Service that manages academic years.")
 
 class AvailablePermissions(AbstractEnumLoader):
     """
@@ -139,78 +157,67 @@ class AvailablePermissions(AbstractEnumLoader):
     """
     CRUD_PROFILE = Permission(AvailableServices.PROFILE_SERVICE.value,
                                 [AvailableOperations.CREATE.value,
-                                 AvailableOperations.GET.value,
+                                 AvailableOperations.GET_MULTIPLE.value,
+                                 AvailableOperations.GET_SINGLE.value,
+                                 AvailableOperations.GET_ME.value,
                                  AvailableOperations.UPDATE.value,
                                  AvailableOperations.DELETE.value])
     CRUD_ACCOUNT = Permission(AvailableServices.ACCOUNT_SERVICE.value,
                                 [AvailableOperations.CREATE.value,
-                                 AvailableOperations.GET.value,
+                                 AvailableOperations.GET_MULTIPLE.value,
+                                 AvailableOperations.GET_SINGLE.value,
+                                 AvailableOperations.GET_ME.value,
                                  AvailableOperations.UPDATE.value,
                                  AvailableOperations.DELETE.value])
     CRUD_STATUS  = Permission(AvailableServices.STATUS_SERVICE.value,
                                 [AvailableOperations.CREATE.value,
-                                 AvailableOperations.GET.value,
+                                 AvailableOperations.GET_MULTIPLE.value,
+                                 AvailableOperations.GET_SINGLE.value,
+                                 AvailableOperations.GET_ME.value,
                                  AvailableOperations.UPDATE.value,
                                  AvailableOperations.DELETE.value])
     CRUD_NODE    = Permission(AvailableServices.NODE_SERVICE.value,
                                 [AvailableOperations.CREATE.value,
-                                AvailableOperations.GET.value,
-                                AvailableOperations.UPDATE.value,
-                                AvailableOperations.DELETE.value])
+                                 AvailableOperations.GET_MULTIPLE.value,
+                                 AvailableOperations.GET_SINGLE.value,
+                                 AvailableOperations.GET_ME.value,
+                                 AvailableOperations.UPDATE.value,
+                                 AvailableOperations.DELETE.value])
     CRUD_UE      = Permission(AvailableServices.UE_SERVICE.value,
                                 [AvailableOperations.CREATE.value,
-                                 AvailableOperations.GET.value,
+                                 AvailableOperations.GET_MULTIPLE.value,
+                                 AvailableOperations.GET_SINGLE.value,
+                                 AvailableOperations.GET_ME.value,
                                  AvailableOperations.UPDATE.value,
                                  AvailableOperations.DELETE.value])
     CRUD_ROLE    = Permission(AvailableServices.ROLE_SERVICE.value,
                                 [AvailableOperations.CREATE.value,
-                                 AvailableOperations.GET.value,
+                                 AvailableOperations.GET_MULTIPLE.value,
+                                 AvailableOperations.GET_SINGLE.value,
+                                 AvailableOperations.GET_ME.value,
                                  AvailableOperations.UPDATE.value,
                                  AvailableOperations.DELETE.value])
     CRUD_AFFECTATION = Permission(AvailableServices.AFFECTATION_SERVICE.value,
                                 [AvailableOperations.CREATE.value,
-                                 AvailableOperations.GET.value,
+                                 AvailableOperations.GET_MULTIPLE.value,
+                                 AvailableOperations.GET_SINGLE.value,
+                                 AvailableOperations.GET_ME.value,
                                  AvailableOperations.UPDATE.value,
                                  AvailableOperations.DELETE.value])
     CRUD_COURSE = Permission(AvailableServices.COURSE_SERVICE.value,
                                  [AvailableOperations.CREATE.value,
-                                  AvailableOperations.GET.value,
-                                  AvailableOperations.UPDATE.value,
-                                  AvailableOperations.DELETE.value])
-
+                                 AvailableOperations.GET_MULTIPLE.value,
+                                 AvailableOperations.GET_SINGLE.value,
+                                 AvailableOperations.GET_ME.value,
+                                 AvailableOperations.UPDATE.value,
+                                 AvailableOperations.DELETE.value])
     CRUD_ACADEMIC_YEAR = Permission(AvailableServices.ACADEMIC_YEAR_SERVICE.value,
-                             [AvailableOperations.CREATE.value,
-                              AvailableOperations.GET.value,
-                              AvailableOperations.UPDATE.value,
-                              AvailableOperations.DELETE.value])
-
-class AvailableEnsemblesPermissions(enum.Enum):
-    """
-    This class regroups the different kind of permissions for the services.
-    """
-    GET_PROFILE     = Permission(AvailableServices.PROFILE_SERVICE.value,
-                                [AvailableOperations.GET.value])
-    MANAGE_PROFILE  = AvailablePermissions.CRUD_ROLE.value
-
-    GET_ACCOUNT     = Permission(AvailableServices.ACCOUNT_SERVICE.value,
-                                [AvailableOperations.GET.value])
-    MANAGE_ACCOUNT  = AvailablePermissions.CRUD_ACCOUNT.value
-
-    GET_STATUS      = Permission(AvailableServices.STATUS_SERVICE.value,
-                                [AvailableOperations.GET.value])
-    MANAGE_STATUS   = AvailablePermissions.CRUD_STATUS.value
-
-    GET_UE          = Permission(AvailableServices.UE_SERVICE.value,
-                                 [AvailableOperations.GET.value])
-    MANAGE_UE       = AvailablePermissions.CRUD_UE.value
-
-    GET_ROLE        = Permission(AvailableServices.ROLE_SERVICE.value,
-                                    [AvailableOperations.GET.value])
-    MANAGE_ROLE     = AvailablePermissions.CRUD_ROLE.value
-
-    GET_AFFECTATION = Permission(AvailableServices.AFFECTATION_SERVICE.value,
-                                [AvailableOperations.GET.value])
-    MANAGE_AFFECTATION = AvailablePermissions.CRUD_AFFECTATION.value
+                                    [AvailableOperations.CREATE.value,
+                                    AvailableOperations.GET_MULTIPLE.value,
+                                    AvailableOperations.GET_SINGLE.value,
+                                    AvailableOperations.GET_ME.value,
+                                    AvailableOperations.UPDATE.value,
+                                    AvailableOperations.DELETE.value])
 
 class AvailableRoles(AbstractEnumLoader):
     """
@@ -218,16 +225,22 @@ class AvailableRoles(AbstractEnumLoader):
     This is static data. It should be stored in the database at 
     startup if not already present.
     """
-    ADMIN           = Role("Administrateur", "Rôle administrateur. Dispose de toutes les permissions.",
-                           None, True)
+    ADMIN           = Role("Administrateur", "Rôle administrateur. Dispose de toutes les permissions pour toutes les années académiques.",
+                           None,
+                           True)
     DPT_MANAGER     = Role("Responsable de département", "Rôle pour gèrer un département.",
-                           None, False)
+                           None,
+                           True)
     FMT_MANAGER     = Role("Responsable de formation", "Rôle pour gèrer une formation.",
-                           None, False)
+                           None,
+                           False)
     ED_SECRETARIAT  = Role("Secrétariat", "Rôle pour le secrétariat.",
-                           None, False)
+                           None,
+                           False)
     TEACHER         = Role("Professeur", "Professeur.",
-                           None, False)
+                           None,
+                           False)
     UNASSIGNED      = Role("Non assigné", "Rôle par défaut.",
-                           None, False)
+                           None,
+                           False)
     # TODO : Add proper Ensemble permissions to roles.
